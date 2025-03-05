4044d9e6b87f0b81df40d5720fbf5638ce69cd3b
Paul Dumais <paul@unstate.ca>
2025-03-05 12:37:12 -0400

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ca.cawala/geotrie
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ca/cawala/geotrie_test.clj
@@ -0,0 +1,7 @@
+(ns ca.cawala.geotrie-test
+  (:require [clojure.test :refer :all]
+            [ca.cawala.geotrie :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
