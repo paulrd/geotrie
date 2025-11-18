4f70e2e067b260e4658354df2e9a891c5175e6d8
Paul Dumais <paul@unstate.ca>
2025-11-18 16:12:48 -0400

    some progress

--- src/ca/cawala/geotrie.clj
+++ src/ca/cawala/geotrie.clj
@@ -1,5 +1,9 @@
 (ns ca.cawala.geotrie
+  (:require [clojure.core.reducers :as r]
+            [clojure.java.io :as io])
   (:import
+   (java.awt Rectangle)
+   (java.awt.image Raster)
    (org.geotools.coverage.grid GridCoordinates2D GridEnvelope2D GridGeometry2D)
    (org.geotools.coverage.grid.io AbstractGridFormat)
    (org.geotools.coverage.processing CoverageProcessor)
@@ -7,56 +11,62 @@
    (org.geotools.geometry Position2D)
    (org.geotools.geometry.jts ReferencedEnvelope)))
 
-(defn sum-area [region min-col min-row max-col max-row]
-  (apply +
-         (for [col (range min-col (inc max-col))
-               row (range min-row (inc max-row))
-               :let [val (first (.evaluate region (GridCoordinates2D. col row) nil))]
-               :when (> val 0)]
-           val)))
+(defn sum-tile [raster min-col min-row max-col max-row]
+  (let [tile-rect (.getBounds raster)
+        region-rect (Rectangle. min-col min-row (- max-col min-col) (- max-row min-row))
+        rect (.intersection tile-rect region-rect)]
+    (apply + (for [x (range (.getWidth rect))
+                   y (range (.getHeight rect))]
+               (.getSampleFloat raster (+ x (.getX rect)) (+ y (.getY rect)) 0)))))
 
-(defn sum-in-area [file min-lon max-lon min-lat max-lat]
-  (let [reader (GeoTiffReader. file)
-        cov (.read reader nil)
-        meters-per-degree 111320
-        crs (.getCoordinateReferenceSystem cov)
-        roi-envelope (ReferencedEnvelope. min-lon max-lon min-lat max-lat crs)
+(defn sum-area [region min-col min-row max-col max-row]
+  (let [img (.getRenderedImage region)
+        minX (.getMinTileX img)
+        minY (.getMinTileY img)
+        tiles (vec (for [x (range minX (+ minX (.getNumXTiles img)))
+                         y (range minY (+ minY (.getNumYTiles img)))]
+                     [x y]))
+        tileWidth (.getTileWidth img)
+        tileHeight (.getTileHeight img)]
+    (->> tiles
+         (r/filter #(not (or (<= (* (inc (first %)) tileWidth) min-col) ; tile is too far left
+                             (> (* (first %) tileWidth) max-col)        ; tile is too far right
+                             (<= (* (inc (last %)) tileHeight) min-row) ; tile is too far above
+                             (> (* (last %) tileHeight) max-row)        ; tile is too far below
+                             )))
+         (r/map #(.getTile img (first %) (last %)))
+         (r/map #(sum-tile % min-col min-row max-col max-row))
+         (r/filter #(< 0 %))
+         (r/fold +))))
+
+(defn to-grid
+  "Converts from lat/lon to grid coordinates."
+  [cov min-lon max-lon min-lat max-lat]
+  (let [crs (.getCoordinateReferenceSystem cov)
         processor (CoverageProcessor/getInstance nil)
         param (.getParameters (.getOperation processor "CoverageCrop"))
         _ (.setValue (.parameter param "Source") cov)
-        _ (.setValue (.parameter param "Envelope") roi-envelope)
-        cropped (.doOperation processor param)
-        grid-geometry (.getGridGeometry cropped)
-        lower-left (Position2D. crs min-lon min-lat)
-        upper-right (Position2D. crs max-lon max-lat)
-        grid-lower-left (.worldToGrid grid-geometry lower-left)
-        grid-upper-right (.worldToGrid grid-geometry upper-right)
+        _ (.setValue (.parameter param "Envelope") (ReferencedEnvelope. min-lon max-lon min-lat max-lat crs))
+        grid-geometry (.getGridGeometry (.doOperation processor param))
+        grid-lower-left (.worldToGrid grid-geometry (Position2D. crs min-lon min-lat))
+        grid-upper-right (.worldToGrid grid-geometry (Position2D. crs max-lon max-lat))
         grid-range (.getGridRange2D grid-geometry)
         min-col (Math/max (.x grid-lower-left) (.x grid-range))
         min-row (Math/max (.y grid-upper-right) (.y grid-range))
         max-col (Math/min (.x grid-upper-right) (+ (.x grid-range) (.width grid-range) -1))
-        max-row (Math/min (.y grid-lower-left) (+ (.y grid-range) (.height grid-range) -1))
-        gg (.createValue AbstractGridFormat/READ_GRIDGEOMETRY2D)
-        grid-envelope-2D (GridEnvelope2D. min-col min-row (- max-col min-col -1) (- max-row min-row -1))
-        _ (.setValue gg (GridGeometry2D. grid-envelope-2D (.getGridToCRS grid-geometry) crs))
-        region (.read reader (into-array [gg]))
-        pop (sum-area region min-col min-row max-col max-row)]
-    (println "region in pixels: " [min-col min-row max-col max-row])
-    (println "population of Newfoundland: " pop)
-    pop))
-
-(def file "global_pop_2026_CN_1km_R2025A_UA_v1.tif")
-
-(defn main
-  "this region should cover the main island of newfoundland it should be around
-  530,000"
-  []
-  (let [path (str "/home/paul/Downloads/" file)]
-    (sum-in-area path -59.963389 -50.976356 46.408949 52.014232)))
-
-;; Data source: worldpop.org.
-;; old file: ppp_2020_1km_Aggregated.tif
+        max-row (Math/min (.y grid-lower-left) (+ (.y grid-range) (.height grid-range) -1))]
+    (Rectangle. min-col min-row (inc (- max-col min-col)) (inc (- max-row min-row)))))
 
 (comment
-  (main)
-  )
+;; Data source: worldpop.org.
+;; old file: ppp_2020_1km_Aggregated.tif
+;; Bounds: -180.0, -59.999999423999995, 179.99999856000005, 84.0
+;; [-180.0 179.99999856000005 -59.999999423999995 84.0]
+  (do
+    (def file-name "global_pop_2026_CN_1km_R2025A_UA_v1.tif")
+    (def meters-per-degree 111320)) ; to use to get approximate dimensions of a 'square' lat/lon
+  (io/resource file-name)
+  (* 85 34)
+  (* 85 512)
+  (* 34 512)
+  :dbg)
--- test/ca/cawala/geotrie_test.clj
+++ test/ca/cawala/geotrie_test.clj
@@ -1,8 +1,64 @@
 (ns ca.cawala.geotrie-test
   (:require [clojure.test :refer :all]
-            [ca.cawala.geotrie :refer :all]))
+            [clojure.java.io :as io]
+            [ca.cawala.geotrie :refer :all])
+  (:import (java.awt Rectangle)
+           (org.geotools.gce.geotiff GeoTiffReader)
+           (org.geotools.coverage.grid GridCoordinates2D GridEnvelope2D GridGeometry2D)))
 
-(deftest area-test
-  (testing "Test population of Newfoundland"
-    (let [pop (main)]
-      (is (and (> 517678 pop) (< 517677 pop))))))
+(def one-place {:val 3.88987e-14 :col 14384 :row 4527
+                :upper-left {:lat 46.274420 :lon -60.132772}
+                :lower-right {:lat 46.267677 :lon -60.126012}})
+(def min-lon (get-in one-place [:upper-left :lon]))
+(def max-lon (get-in one-place [:lower-right :lon]))
+(def min-lat (get-in one-place [:lower-right :lat]))
+(def max-lat (get-in one-place [:upper-left :lat]))
+(def file-name "global_pop_2026_CN_1km_R2025A_UA_v1.tif")
+(def cov (.read (GeoTiffReader. (clojure.java.io/resource file-name)) nil))
+
+(deftest to-grid-test
+  (testing "Test conversion from lat/lon area to grid area."
+    (is (= (Rectangle. 0 0 43200 17280)
+           (to-grid cov -180 180 -90 90)))
+    (is (= (Rectangle. (:col one-place) (:row one-place) 1 1)
+           (to-grid cov min-lon max-lon min-lat max-lat)))))
+
+(deftest y-grid-is-north
+  (testing "Test to see that if we go up in y, we go north."
+    (let [x1 (:col one-place)
+          x2 (inc (:col one-place))
+          y1 (:row one-place)
+          y2 (inc (:row one-place))]
+      (is (and (< (first (.evaluate cov (GridCoordinates2D. x1 y1) (make-array Float/TYPE 1)))
+                  3.8898729E-14)
+               (> (first (.evaluate cov (GridCoordinates2D. x2 y2) (make-array Float/TYPE 1)))
+                  3.8898727E-14))))))
+
+(defn get-tiles
+  "Takes a coverage object and a rectangle and returns a set of tiles containing these points."
+  [cov rect]
+  (let [img (.getRenderedImage cov)
+        minX (.getMinTileX img)
+        minY (.getMinTileY img)
+        tiles (vec (for [x (range minX (+ minX (.getNumXTiles img)))
+                         y (range minY (+ minY (.getNumYTiles img)))]
+                     [x y]))
+        tileWidth (.getTileWidth img)
+        tileHeight (.getTileHeight img)]
+    (->> tiles
+         (r/filter #(not (or (<= (* (inc (first %)) tileWidth) min-col) ; tile is too far left
+                             (> (* (first %) tileWidth) max-col)        ; tile is too far right
+                             (<= (* (inc (last %)) tileHeight) min-row) ; tile is too far above
+                             (> (* (last %) tileHeight) max-row)        ; tile is too far below
+                             )))
+         (r/map #(.getTile img (first %) (last %)))
+         (r/map #(sum-tile % min-col min-row max-col max-row))
+         (r/filter #(< 0 %))
+         (r/fold +))))
+
+(deftest get-tiles
+  (testing "get tile rasters for grid range"
+    (to-grid cov min-lon max-lon min-lat max-lat)
+
+    )
+  )
