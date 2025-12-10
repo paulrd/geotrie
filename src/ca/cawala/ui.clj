9877cfd55b983df4adb8451a9e4e5aa35b179369
Paul Dumais <paul@unstate.ca>
2025-12-10 19:50:55 -0400

    working java example with clojure and swing

--- src/ca/cawala/ui.clj
+++ src/ca/cawala/ui.clj
@@ -1,10 +1,24 @@
 (ns ca.cawala.ui
-  (:require [org.corfield.logging4j2 :as logger]))
+  (:require [org.corfield.logging4j2 :as logger]
+            [clojure.java.io :as io]
+            [seesaw.core :as s])
+  (:import [org.jxmapviewer.examples Examples]
+           [org.jxmapviewer.examples.controller MainController]
+           [org.jxmapviewer Globals]
+           ))
 
-(defn -main [& args]
-
-  )
+(defn -main [& _]
+  (s/native!)
+  (let [frame (s/frame :on-close :dispose :title "JXMapViewer2 Examples"
+                       :icon (io/resource "images/app.png")
+                       :width Globals/WIDTH :height Globals/HEIGHT)]
+    (MainController. frame)
+    (s/show! frame)))
 
 (comment
-  (logger/info "Hello World Again")
-  )
+  (-main)
+  (logger/with-log-context {:id "tony"}
+    (logger/info "Hello World Again"))
+  (logger/with-log-tag "hungry"
+    (logger/info "Hello World Again"))
+  "great")
